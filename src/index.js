789cb7341910c5c53bfd02bc30aa1d79db03c4c0
Evgen <Evgeniy.shmat@gmail.com>
2021-03-15 18:22:50 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -9,12 +9,19 @@ exports.min = function min(array=[]) {
   }
 };
 
-exports.max = function max(array) {
-  return 0;
+exports.max = function max(array=[]) {
+   if (array.length !== 0) {
+       return Math.max.apply(null, array);
+   } else {
+       return 0;
+   }
 };
 
-exports.avg = function avg(array) {
+exports.avg = function avg(array = []) {
+  if (array.length !== 0){
+     return array.reduce((a, b) => (a + b)) / array.length;
+} else {
   return 0;
+ }
 };
-
 
